af2bb31c4aae28804e7f6a8201298469bee18881
Sara-Rubaya <sararubaya4800@gmail.com>
2025-07-01 21:28:01 +0600

    added rider role

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ async function run() {
 
         app.patch("/riders/:id/status", async (req, res) => {
             const { id } = req.params;
-            const { status } = req.body;
+            const { status, email } = req.body;
             const query = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set:
@@ -188,6 +188,21 @@ async function run() {
                     query, updateDoc
 
                 );
+
+                //update user role for accepting rider
+                if(status === 'active'){
+                    const useQuery = {email};
+                    const userUpdateDoc = {
+                        $set:{
+                            role: 'rider'
+                        }
+                    };
+                
+                    const roleResult =await usersCollection.updateOne(useQuery,userUpdateDoc)
+                    console.log(roleResult.modifiedCount)
+                }
+
+
                 res.send(result);
             } catch (err) {
                 res.status(500).send({ message: "Failed to update rider status" });
